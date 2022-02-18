4eda9225aeb6c10cc1e42edca36523d8cf0a8552
tapusd <sdtapusd20@gmail.com>
2022-02-18 23:36:36 +0600

    solved challenge 7

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,15 @@
 package com.redislabs.university.RU102J.dao;
 
+import com.redislabs.university.RU102J.core.KeyHelper;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Random;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -19,6 +28,20 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     @Override
     public void hit(String name) throws RateLimitExceededException {
         // START CHALLENGE #7
+        try(Jedis jedis = jedisPool.getResource();
+            Transaction tx = jedis.multi()) {
+            final String slidingWindowKey = KeyHelper.getKey("limiter:" + windowSizeMS + ":" + name + ":" + maxHits);
+            final long timestamp = ZonedDateTime.now().toInstant().toEpochMilli();
+            final String member = timestamp + "-" + Math.random();
+            tx.zadd(slidingWindowKey, timestamp, member);
+            tx.zremrangeByScore(slidingWindowKey, 0,  (timestamp - windowSizeMS));
+            final Response<Long> hit = tx.zcard(slidingWindowKey);
+            tx.exec();
+
+            if (hit.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
         // END CHALLENGE #7
     }
 }
