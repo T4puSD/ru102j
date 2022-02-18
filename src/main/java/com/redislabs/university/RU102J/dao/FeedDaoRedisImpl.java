f10ab98da8f6489e5fda8d9cbad5093d8e8753f3
tapusd <sdtapusd20@gmail.com>
2022-02-18 17:31:54 +0600

    solved challenge 6

--- src/main/java/com/redislabs/university/RU102J/dao/FeedDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/FeedDaoRedisImpl.java
@@ -6,6 +6,7 @@ import redis.clients.jedis.*;
 import java.nio.channels.Pipe;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FeedDaoRedisImpl implements FeedDao {
@@ -22,6 +23,13 @@ public class FeedDaoRedisImpl implements FeedDao {
     @Override
     public void insert(MeterReading meterReading) {
         // START Challenge #6
+        try(Jedis jedis = jedisPool.getResource()) {
+            final Map<String, String> meterReadingMap = meterReading.toMap();
+            jedis.xadd(RedisSchema.getGlobalFeedKey(), StreamEntryID.NEW_ENTRY,
+                    meterReadingMap, globalMaxFeedLength, true);
+            jedis.xadd(RedisSchema.getFeedKey(meterReading.getSiteId()), StreamEntryID.NEW_ENTRY,
+                    meterReadingMap, siteMaxFeedLength, true);
+        }
         // END Challenge #6
     }
 
